7a22013be992ff52d7b317b4281552820fc3627c
gabriel_souza_rbm <gabriel.souza@rbmweb.com.br>
2022-10-03 09:22:17 -0300

    duas versoes

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const data = require("./recipesV2.json");
+const dataOriginal = require("./recipes.json");
 const app = require('express')();
 // const { v4 } = require('uuid');
 
@@ -10,9 +11,17 @@ app.get('/api', (req, res) => {
   res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-  res.send(data);
+  res.send(dataOriginal);
 });
 
+app.get('/apiV2', (req, res) => {
+  res.setHeader( 'Access-Control-Allow-Origin', '*');
+  res.setHeader( 'Access-Control-Allow-Credentials', true);
+  res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.send(data);
+});
 
 
 module.exports = app;
