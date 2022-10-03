6d6d2d0280a196002aaadbb72427235c8d4f4c09
gabriel_souza_rbm <gabriel.souza@rbmweb.com.br>
2022-10-03 09:17:56 -0300

    .

--- api/index.js
+++ api/index.js
@@ -2,11 +2,17 @@ const data = require("./recipesV2.json");
 const app = require('express')();
 // const { v4 } = require('uuid');
 
+
+
 app.get('/api', (req, res) => {
+  res.setHeader( 'Access-Control-Allow-Origin', '*');
+  res.setHeader( 'Access-Control-Allow-Credentials', true);
+  res.setHeader( 'Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.send(data);
 });
+
 
 
 module.exports = app;
