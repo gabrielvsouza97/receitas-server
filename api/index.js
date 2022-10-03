bfc5ae5ae60da030eaa25ef6ef9f871eab141a40
gabriel_souza_rbm <gabriel.souza@rbmweb.com.br>
2022-10-03 09:28:22 -0300

    .

--- api/index.js
+++ api/index.js
@@ -3,7 +3,24 @@ const dataOriginal = require("./recipes.json");
 const app = require('express')();
 // const { v4 } = require('uuid');
 
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.get('/api', (req, res) => {
   res.setHeader( 'Access-Control-Allow-Origin', '*');
