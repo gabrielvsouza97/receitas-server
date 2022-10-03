02d9046a173e7b11d514036120914887c198313b
gabriel_souza_rbm <gabriel.souza@rbmweb.com.br>
2022-10-03 08:58:30 -0300

    first commit

---
+++ api/index.js
@@ -0,0 +1,12 @@
+import data from "./recipesV2.json" assert{type: "json"};
+const app = require('express')();
+const { v4 } = require('uuid');
+
+app.get('/api', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  res.send(data);
+});
+
+
+module.exports = app;
